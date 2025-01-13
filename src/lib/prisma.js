d8d7c3d008ae4d0df12fef463695be29e86444d7
Niteen Kumar <niteen22336@iiitd.ac.in>
2025-01-13 23:05:54 +0530

    updated compose.yaml

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,9 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
-}
+const prisma = new PrismaClient({ datasources: {  db: { url: process.env.DATABASE_URL } } });
 
 export default prisma;
