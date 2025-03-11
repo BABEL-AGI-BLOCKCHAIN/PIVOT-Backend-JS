5892588ad468f845cfc7c71eb1478ca9824d9386
Niteen Kumar <niteen22336@iiitd.ac.in>
2025-03-11 20:05:15 +0530

    feat: updated abi

--- src/events/eventListener.event.js
+++ src/events/eventListener.event.js
@@ -4,14 +4,14 @@ import listenToInvest from './invest.event.js';
 import getHistoricInvests from './getHistoricInvests.event.js';
 import cron from 'node-cron';
 
-const initEventListeners = async () => {
+const initEventListeners = () => {
   listenToCreateTopic();
   listenToInvest();
-  await getHistoricTopics();
-  await getHistoricInvests();
-  cron.schedule('*/2 * * * *', async () => {
-      await getHistoricTopics();
-      await getHistoricInvests();
+  getHistoricTopics();
+  getHistoricInvests();
+  cron.schedule('*/2 * * * *', () => {
+      getHistoricTopics();
+      getHistoricInvests();
   });
 };
 
