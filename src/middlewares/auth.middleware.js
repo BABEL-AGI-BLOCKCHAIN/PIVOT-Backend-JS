ca7fe974923423a7be879764651074b9652da23d
Xander <x20297782@gmail.com>
2025-04-09 12:51:19 +0800

    fix: change apiError to Error

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,40 +1,34 @@
 import jwt from "jsonwebtoken";
 import prisma from "../utils/prisma.js";
 
-
-
-const verifyJWT = async (req, _ , next) => {
+const verifyJWT = async (req, res, next) => {
     try {
-        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "") ;
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
         if (!token) {
-            throw new apiError(401 , "Unauthorized request");
+            throw new Error("Unauthorized request");
         }
-    
-        const isAuthorized = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET) ;
-    
-        const user = await prisma.user.findUnique (isAuthorized.id).select(
-            "-password -refreshToken"
-        )
+
+        const isAuthorized = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+        const user = await prisma.user.findUnique(isAuthorized.id).select("-password -refreshToken");
 
         if (!user) {
-            throw new error(401 ,"Unauthorized request");
+            throw new Error("Unauthorized request");
         }
 
-        req.user = user ;
+        req.user = user;
         next();
-        
     } catch (error) {
-        throw new error(401 , error.message || "Unauthorized request")
+        // throw new error(401, error.message || "Unauthorized request");
+        return res.status(401).json({ message: error.message || "Unauthorized request" });
     }
-
 };
 
-
 const internalAuthMiddleware = (req, res, next) => {
-    const secret = req.headers['internal-secret'];
+    const secret = req.headers["internal-secret"];
 
     if (!secret || secret !== process.env.INTERNAL_SECRET) {
-        return res.status(403).json({ message: 'Forbidden' });
+        return res.status(403).json({ message: "Forbidden" });
     }
 
     next();
