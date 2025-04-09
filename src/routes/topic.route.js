c2dedad96bb05cf0fa7c5e2771a311bd64e777b1
Xander <x20297782@gmail.com>
2025-04-09 12:51:07 +0800

    fix: verifyJWT for updateTopic and comment

--- src/routes/topic.route.js
+++ src/routes/topic.route.js
@@ -2,31 +2,34 @@ import { Router } from "express";
 
 const router = Router();
 
-
 import { getTopics, getTopicById, getTopicsByUser, createTopic, updateTopic, getComments, comment } from "../controllers/topic.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { internalAuthMiddleware } from "../middlewares/auth.middleware.js";
+import { internalAuthMiddleware, verifyJWT } from "../middlewares/auth.middleware.js";
 
-router.route('/getTopics').get(getTopics);
+router.route("/getTopics").get(getTopics);
 
-router.route('/getTopic/:id').get(getTopicById);
+router.route("/getTopic/:id").get(getTopicById);
 
-router.route('/getTopics/user/:userId').get(getTopicsByUser);
+router.route("/getTopics/user/:userId").get(getTopicsByUser);
 
-router.route('/createTopic')
-  // #swagger.ignore = true
-  .post(internalAuthMiddleware, createTopic);
+router
+    .route("/createTopic")
+    // #swagger.ignore = true
+    .post(internalAuthMiddleware, createTopic);
 
+router.route("/updateTopic").post(
+    verifyJWT,
+    upload.fields([
+        {
+            name: "image",
+            maxCount: 1,
+        },
+    ]),
+    updateTopic
+);
 
-router.route('/updateTopic').post(upload.fields([
-    {
-        name: 'image',
-        maxCount: 1
-    }
-]), updateTopic);
+router.route("/getComments/:topicId").get(getComments);
 
-router.route('/getComments/:topicId').get(getComments);
-
-router.route('/comment').post(comment);
+router.route("/comment").post(verifyJWT, comment);
 
 export default router;
